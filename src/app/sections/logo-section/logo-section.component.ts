5188fbf5ae602893ccc82ab3731c526f02ca58b0
Lam Nguyen <lam.nguyen@oursky-sea.com>
2019-10-30 22:13:45 +0700

    added logo section

--- src/app/sections/logo-section/logo-section.component.ts
+++ src/app/sections/logo-section/logo-section.component.ts
@@ -1,8 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+
+export interface LogoData {
+  title: string;
+  img: {
+    src: string;
+    alt: string;
+  }
+}
 
 @Component({
   selector: 'logo-section',
   templateUrl: 'logo-section.component.html',
   styleUrls: ['logo-section.component.scss']
 })
-export class LogoSection {}
+export class LogoSection {
+  @Input() data: Array<LogoData> = [];
+}
--- src/app/sections/sections.component.ts
+++ src/app/sections/sections.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogoData } from "./logo-section/logo-section.component";
 
 @Component({
   selector: 'app-sections',
@@ -6,8 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sections.component.css']
 })
 export class SectionsComponent implements OnInit {
-  focus;
-  focus1;
+
+  logoSection: Array<LogoData> = [
+    {
+      title: 'Conversity',
+      img: {
+        src: './assets/img/theme/team-4-800x800.jpg',
+        alt: 'Conversity'
+      }
+    },
+    {
+      title: 'Conversity',
+      img: {
+        src: './assets/img/theme/team-4-800x800.jpg',
+        alt: 'Conversity'
+      }
+    },
+    {
+      title: 'Conversity',
+      img: {
+        src: './assets/img/theme/team-4-800x800.jpg',
+        alt: 'Conversity'
+      }
+    },
+    {
+      title: 'Conversity',
+      img: {
+        src: './assets/img/theme/team-4-800x800.jpg',
+        alt: 'Conversity'
+      }
+    },
+    {
+      title: 'Conversity',
+      img: {
+        src: './assets/img/theme/team-4-800x800.jpg',
+        alt: 'Conversity'
+      }
+    },
+    {
+      title: 'Conversity',
+      img: {
+        src: './assets/img/theme/team-4-800x800.jpg',
+        alt: 'Conversity'
+      }
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
